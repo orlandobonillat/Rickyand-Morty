71a5f949d8fb0f99fdde3dbb3d53182e8f67b80e
orlandb2 <orlando.bonillat@gmail.com>
2024-08-26 17:53:50 -0500

    feat: Add button to back home

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-import imageRickMorty from './img/ricky-morty.png';
-import './App.css';
-import { useState } from 'react';
-import Characters from './components/Characters';
+import imageRickMorty from "./img/ricky-morty.png";
+import "./App.css";
+import { useState } from "react";
+import Characters from "./components/Characters";
 
 function App() {
-
   const [characters, setCharacters] = useState(null);
 
-
   const reqApi = async () => {
-    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const api = await fetch("https://rickandmortyapi.com/api/character");
     const characterApi = await api.json();
     setCharacters(characterApi.results);
-  }
-
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Rick and Morty</h1>
-        {characters ?
-          (<Characters characters={characters} />) :
-          (
-            <>
-              <img src={imageRickMorty} alt="Rick and Morty" className="img-home" />
-              <button onClick={reqApi} className="btn-search">Buscar Personajes</button>
-            </>
-          )}
-
-
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img
+              src={imageRickMorty}
+              alt="Rick and Morty"
+              className="img-home"
+            />
+            <button onClick={reqApi} className="btn-search">
+              Buscar Personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
